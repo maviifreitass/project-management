d5138dd010549b5a64146f488b87410a8e9479d9
maviifreitass <mariavitoria.freitas@yahoo.com>
2025-01-30 16:24:38 -0300

    feat: tela de login de usuários

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,30 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\ProjectController;
-use App\Http\Controllers\ClientController;
-use App\Http\Controllers\TaskController;
-
-Route::resource('users', UserController::class);
-Route::resource('projects', ProjectController::class);
-Route::resource('clients', ClientController::class);
-Route::resource('tasks', TaskController::class);
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+use App\Http\Controllers\AuthController;
+
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::get('/dashboard', function () {
+    return 'Bem-vindo ao Dashboard!';
+})->middleware('auth')->name('dashboard');
+
+
+require __DIR__.'/auth.php';
